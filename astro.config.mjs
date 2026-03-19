49760e91ffbf4a39d4a4d66f18c195b2e0f977d2
Billy (Portia Worker) <portia@mypa.chat>
2026-03-19 12:19:29 -0400

    Add sitemap.xml via @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
-  output: 'static',
-  site: 'https://robertsonprice.com',
+  integrations: [mdx(), sitemap()],
+  output: "static",
+  site: "https://robertsonprice.com",
 });
